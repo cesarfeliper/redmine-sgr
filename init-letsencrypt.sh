e87bb17805572d21af448b231138b8ed9ce50c9f
cesarfeliper <cfeliper@indracompany.com>
2020-08-10 10:12:12 -0300

    novo teste

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(semge.test www.semge.test)
+domains=(semge.indrabrasil.com.br www.semge.indrabrasil.com.br)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
